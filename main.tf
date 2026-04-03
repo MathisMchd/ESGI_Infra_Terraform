5c45c4212f0ec1731139eb0c5f2b987880228f1e
calulazone <lutinelo.lg@gmail.com>
2026-04-03 16:10:12 +0200

    tf conf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "projet-terraform"
+  }
+
+  user_data = file("UserdataScript-phase-2.sh")
+}
