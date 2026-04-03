4dee401e55c43a6de242b8ae23f50a7c27ef82cc
MathisMchd <michenaudmathis@gmail.com>
2026-04-03 16:34:58 +0200

    security group

--- main.tf
+++ main.tf
@@ -2,24 +2,59 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "security_group" {
+  name        = "security_group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  # ingress {
+  #   from_port   = 22
+  #   to_port     = 22
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/32"] # SSH
+  # }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # HTTP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
-
-  owners = ["099720109477"]
 }
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.security_group.id]
 
   tags = {
     Name = "projet-terraform"
   }
 
   user_data = file("UserdataScript-phase-2.sh")
+}
+
+output "public_ip" {
+  value = aws_instance.app_server.public_ip
 }
